c4b62a440c2d47b382200adde40dfb0ea8f1b7a8
yerycs <yerycs1015@gmail.com>
2025-08-25 09:42:37 +0800

    fix: unused lint error

--- packages/builder/src/settings/pages/general.svelte
+++ packages/builder/src/settings/pages/general.svelte
@@ -10,7 +10,7 @@
     appStore,
     deploymentStore,
     isOnlyUser,
-    recaptchaStore,
+    // recaptchaStore,
   } from "@/stores/builder"
   import { admin } from "@/stores/portal/admin"
   import { licensing } from "@/stores/portal/licensing"
@@ -22,7 +22,7 @@
     Icon,
     Layout,
     Modal,
-    notifications,
+    // notifications,
   } from "@budibase/bbui"
 
   let versionModal: VersionModal
@@ -35,22 +35,22 @@
 
   $: updateAvailable = $appStore.upgradableVersion !== $appStore.version
   $: revertAvailable = $appStore.revertableVersion != null
-  $: appRecaptchaEnabled = $recaptchaStore.enabled
+  // $: appRecaptchaEnabled = $recaptchaStore.enabled
 
   const exportApp = (opts: { published: any }) => {
     exportPublishedVersion = !!opts?.published
     exportModal.show()
   }
 
-  const updateRecaptcha = async () => {
-    try {
-      const newState = !appRecaptchaEnabled
-      await recaptchaStore.setState(newState)
-      notifications.success(`Recaptcha ${newState ? "enabled" : "disabled"}`)
-    } catch (err: any) {
-      notifications.error(`Failed to set recaptcha state: ${err.message}`)
-    }
-  }
+  // const updateRecaptcha = async () => {
+  //   try {
+  //     const newState = !appRecaptchaEnabled
+  //     await recaptchaStore.setState(newState)
+  //     notifications.success(`Recaptcha ${newState ? "enabled" : "disabled"}`)
+  //   } catch (err: any) {
+  //     notifications.error(`Failed to set recaptcha state: ${err.message}`)
+  //   }
+  // }
 </script>
 
 <Layout noPadding>
