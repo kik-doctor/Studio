f6486f1237554854ac44f02b15d99ae9771a865c
yerycs <yerycs1015@gmail.com>
2025-09-11 02:51:32 +0800

    fix: webhook idempotent, remove profile components

--- packages/frontend-core/src/components/ProfileModal.svelte
+++ packages/frontend-core/src/components/ProfileModal.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { writable } from "svelte/store"
-  import { ModalContent, Body, Input, notifications } from "@budibase/bbui"
+  import { ModalContent, Input, notifications } from "@budibase/bbui"
   import type { User, ContextUser } from "@budibase/types"
   import type { APIClient } from "@budibase/frontend-core"
   import { createEventDispatcher } from "svelte"
@@ -30,10 +30,10 @@
 </script>
 
 <ModalContent title="My profile" confirmText="Save" onConfirm={updateInfo}>
-  <Body size="S">
-    Personalise the platform by adding your first name and last name.
-  </Body>
+<!--  <Body size="S">-->
+<!--    Personalise the platform by adding your first name and last name.-->
+<!--  </Body>-->
   <Input disabled value={user?.email || ""} label="Email" />
-  <Input bind:value={$values.firstName} label="First name" />
-  <Input bind:value={$values.lastName} label="Last name" />
+<!--  <Input bind:value={$values.firstName} label="First name" />-->
+<!--  <Input bind:value={$values.lastName} label="Last name" />-->
 </ModalContent>
