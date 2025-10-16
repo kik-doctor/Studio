40c60ab92700c6e4480e3f305d4ced1c5f3b6393
yerycs <yerycs1015@gmail.com>
2025-10-16 20:04:00 +0800

    refactor: disable github actions

--- packages/builder/src/settings/pages/general.svelte
+++ packages/builder/src/settings/pages/general.svelte
@@ -239,21 +239,21 @@
 <!--    {/if}-->
 <!--  </div>-->
 <!--  <Divider noMargin />-->
-  <Heading size="XS">Danger zone</Heading>
-  <div class="row">
-    <Button
-      warning
-      disabled={!$isOnlyUser}
-      on:click={() => {
-        deleteModal.show()
-      }}
-      tooltip={$isOnlyUser
-        ? undefined
-        : "Unavailable - another user is editing this workspace"}
-    >
-      Delete workspace
-    </Button>
-  </div>
+<!--  <Heading size="XS">Danger zone</Heading>-->
+<!--  <div class="row">-->
+<!--    <Button-->
+<!--      warning-->
+<!--      disabled={!$isOnlyUser}-->
+<!--      on:click={() => {-->
+<!--        deleteModal.show()-->
+<!--      }}-->
+<!--      tooltip={$isOnlyUser-->
+<!--        ? undefined-->
+<!--        : "Unavailable - another user is editing this workspace"}-->
+<!--    >-->
+<!--      Delete workspace-->
+<!--    </Button>-->
+<!--  </div>-->
 </Layout>
 
 <VersionModal bind:this={versionModal} hideIcon={true} />
